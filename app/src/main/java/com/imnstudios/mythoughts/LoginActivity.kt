17c586a55a34aa2570845242e98eb1e0d41f3e5e
imnithish <imnithish@live.com>
2020-06-17 01:04:42 +0530

    UI/UX

--- app/src/main/java/com/imnstudios/mythoughts/LoginActivity.kt
+++ app/src/main/java/com/imnstudios/mythoughts/LoginActivity.kt
@@ -2,6 +2,10 @@ package com.imnstudios.mythoughts
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -9,10 +13,25 @@ import com.imnstudios.mythoughts.utils.AppThemeMode
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var slideDownAnim: Animation
+    lateinit var fadeInAnim: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //setting up animation starts here
+        slideDownAnim =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.slide_down_animation)
+        fadeInAnim = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in_animation)
+        heading.animation = slideDownAnim
+        Handler().postDelayed({
+//            mode_switch.visibility = View.VISIBLE
+//            mode_switch.animation = fadeInAnim
+            log_in.visibility = View.VISIBLE
+            log_in.animation = fadeInAnim
+        }, 1000)
+        //setting up animation ends here
 
         //setting up AppThemeMode starts here
         val appSettingPrefs: SharedPreferences = getSharedPreferences("AppThemeModePrefs", 0)
@@ -20,17 +39,17 @@ class LoginActivity : AppCompatActivity() {
         if (!isNightModeOn) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
-
-        mode_switch.isChecked = isNightModeOn
-        mode_switch.setOnCheckedChangeListener { _: CompoundButton, isNightModeOnFlag: Boolean ->
-            if (isNightModeOnFlag) {
-                val appTheme = AppThemeMode(true, applicationContext)
-                appTheme.setTheme()
-            } else {
-                val appTheme = AppThemeMode(false, applicationContext)
-                appTheme.setTheme()
-            }
-        }
+//
+//        mode_switch.isChecked = isNightModeOn
+//        mode_switch.setOnCheckedChangeListener { _: CompoundButton, isNightModeOnFlag: Boolean ->
+//            if (isNightModeOnFlag) {
+//                val appTheme = AppThemeMode(true, applicationContext)
+//                appTheme.setTheme()
+//            } else {
+//                val appTheme = AppThemeMode(false, applicationContext)
+//                appTheme.setTheme()
+//            }
+//        }
         //setting up AppThemeMode ends here
     }
 }
