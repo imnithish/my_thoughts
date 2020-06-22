d69c4e050ed7bf312576ec90a177a94d5b3a0afe
imnithish <imnithish@live.com>
2020-06-22 14:18:59 +0530

    ui

--- app/src/main/java/com/imnstudios/mythoughts/ui/home/adapters/AllThoughtsAdapter.kt
+++ app/src/main/java/com/imnstudios/mythoughts/ui/home/adapters/AllThoughtsAdapter.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
